c15e7955be89817a8145b5287a646ed6e6d5300b
Linh <beondl20@gmail.com>
2022-01-10 17:52:16 +0700

    upload

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         String msg = "My name is Linh, nice to meet you!";
         System.out.println(msg);
+        System.out.println("Hello");
     }
 }
